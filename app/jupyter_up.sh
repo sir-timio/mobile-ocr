e3dd5f3735a6341537ac1c00db3ed2dde8ace2e9
sir-timio <t-ionov@list.ru>
2022-09-09 16:49:07 +0300

    reduce 100 port to 2

--- app/jupyter_up.sh
+++ app/jupyter_up.sh
@@ -1,1 +1,1 @@
-jupyter notebook --no-browser --allow-root --ip=0.0.0.0 --port=8001 --NotebookApp.token='timur' --NotebookApp.iopub_data_rate_limit=1.0e10
+jupyter notebook --no-browser --allow-root --ip=0.0.0.0 --port=8000 --NotebookApp.token='timur' --NotebookApp.iopub_data_rate_limit=1.0e10
